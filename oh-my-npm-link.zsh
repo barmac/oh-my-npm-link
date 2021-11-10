9378a8e6154da5b74ac4c727bc86adbc30799f28
Maciej Barelkowski <maciej.barelkowski@camunda.com>
2021-11-10 14:25:50 +0100

    WIP

--- oh-my-npm-link.zsh
+++ oh-my-npm-link.zsh
@@ -1,14 +1,43 @@
 #compdef
 
 function autocomplete() {
-    package=$(cat ./package.json 2> /dev/null)
+    local curcontext="$curcontext" state line  ret=1
 
-    if [[ $package != '' ]]
-    then
-        deps=$(echo $package | jq -r '.dependencies | keys[]' 2> /dev/null)
-        compadd -- ${=deps}
-    fi
+    local -a _1st_arguments
+    _1st_arguments=(
+    'link:Link dependency'
+    )
+
+    _arguments -C \
+    '1: :->cmds' \
+    '*: :->args' && ret=0
+
+    __deps() {
+    cat ./package.json &> /dev/null | jq -r ".dependencies | keys[]" 2> /dev/null
+    }
+
+    case $state in
+    cmds)
+        compadd 'link' && ret=0
+        ;;
+
+    args)
+        case $words[2] in
+        (link)
+
+            compadd $(__deps) && ret=0
+            ;;
+        *)
+            ret=0
+            # (( ret )) && _message 'no more arguments'
+            ;;
+
+        esac
+        ;;
+    esac
+
+    return ret
 }
 
-compdef autocomplete npm link
-compdef autocomplete yarn link
+compdef autocomplete npm
+compdef autocomplete yarn
